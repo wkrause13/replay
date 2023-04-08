e991c491405541d5df83cdb2c595f97031e49a7a
Will Krause <will.krause@circonus.com>
2023-04-08 10:34:23 -0400

    use only the performance metrics from lighthouse

--- src/lighthouse/LighthouseRunnerExtension.ts
+++ src/lighthouse/LighthouseRunnerExtension.ts
@@ -14,69 +14,90 @@
     limitations under the License.
  */
 
-import { PuppeteerRunnerExtension } from '../PuppeteerRunnerExtension.js';
-import type { Step, UserFlow } from '../Schema.js';
-import { StepType } from '../Schema.js';
-import { isMobileFlow, isNavigationStep } from './helpers.js';
-
-export class LighthouseRunnerExtension extends PuppeteerRunnerExtension {
-  #isTimespanRunning = false;
-  #isNavigationRunning = false;
-  #lhFlow?: import('lighthouse').UserFlow;
-
-  async createFlowResult(): Promise<import('lighthouse').FlowResult> {
-    if (!this.#lhFlow) {
-      throw new Error('Cannot get flow result before running the flow');
-    }
-    return this.#lhFlow.createFlowResult();
-  }
-
-  override async beforeAllSteps(flow: UserFlow) {
-    await super.beforeAllSteps?.(flow);
-
-    const { startFlow, desktopConfig } = await import('lighthouse');
-
-    let config = undefined;
-    if (!isMobileFlow(flow)) {
-      config = desktopConfig;
-    }
-
-    this.#lhFlow = await startFlow(this.page, {
-      config,
-      flags: { screenEmulation: { disabled: true } },
-      name: flow.title,
-    });
-  }
-
-  override async beforeEachStep(step: Step, flow?: UserFlow) {
-    await super.beforeEachStep?.(step, flow);
-    if (step.type === StepType.SetViewport) return;
-
-    if (isNavigationStep(step)) {
-      if (this.#isTimespanRunning) {
-        await this.#lhFlow!.endTimespan();
-        this.#isTimespanRunning = false;
+    import { PuppeteerRunnerExtension } from '../PuppeteerRunnerExtension.js';
+    import type { Step, UserFlow } from '../Schema.js';
+    import { StepType } from '../Schema.js';
+    import { isMobileFlow, isNavigationStep } from './helpers.js';
+    
+    export class LighthouseRunnerExtension extends PuppeteerRunnerExtension {
+      #isTimespanRunning = false;
+      #isNavigationRunning = false;
+      #lhFlow?: import('lighthouse').UserFlow;
+    
+      async createFlowResult(): Promise<import('lighthouse').FlowResult> {
+        if (!this.#lhFlow) {
+          throw new Error('Cannot get flow result before running the flow');
+        }
+        return this.#lhFlow.createFlowResult();
       }
-      await this.#lhFlow!.startNavigation();
-      this.#isNavigationRunning = true;
-    } else if (!this.#isTimespanRunning) {
-      await this.#lhFlow!.startTimespan();
-      this.#isTimespanRunning = true;
+    
+      override async beforeAllSteps(flow: UserFlow) {
+        await super.beforeAllSteps?.(flow);
+    
+        const { startFlow, desktopConfig } = await import('lighthouse');
+    
+        let config = undefined;
+        if (!isMobileFlow(flow)) {
+          config = desktopConfig;
+        }
+    
+        this.#lhFlow = await startFlow(this.page, {
+          config,
+          flags: {
+            screenEmulation: { disabled: true },
+            onlyAudits: [
+              'first-contentful-paint',
+              'largest-contentful-paint',
+              'first-meaningful-paint',
+              'speed-index',
+              'final-screenshot',
+              'total-blocking-time',
+              'max-potential-fid',
+              'cumulative-layout-shift',
+              'server-response-time',
+              'interactive',
+              'redirects',
+              'bootup-time',
+              'network-rtt',
+              'network-server-latency',
+              'total-byte-weight',
+              'dom-size',
+            ],
+          },
+          name: flow.title,
+        });
+      }
+    
+      override async beforeEachStep(step: Step, flow?: UserFlow) {
+        await super.beforeEachStep?.(step, flow);
+        if (step.type === StepType.SetViewport) return;
+    
+        if (isNavigationStep(step)) {
+          if (this.#isTimespanRunning) {
+            await this.#lhFlow!.endTimespan();
+            this.#isTimespanRunning = false;
+          }
+          await this.#lhFlow!.startNavigation();
+          this.#isNavigationRunning = true;
+        } else if (!this.#isTimespanRunning) {
+          await this.#lhFlow!.startTimespan();
+          this.#isTimespanRunning = true;
+        }
+      }
+    
+      override async afterEachStep(step: Step, flow?: UserFlow) {
+        if (this.#isNavigationRunning) {
+          await this.#lhFlow!.endNavigation();
+          this.#isNavigationRunning = false;
+        }
+        await super.afterEachStep?.(step, flow);
+      }
+    
+      override async afterAllSteps(flow: UserFlow) {
+        if (this.#isTimespanRunning) {
+          await this.#lhFlow!.endTimespan();
+        }
+        await super.afterAllSteps?.(flow);
+      }
     }
-  }
-
-  override async afterEachStep(step: Step, flow?: UserFlow) {
-    if (this.#isNavigationRunning) {
-      await this.#lhFlow!.endNavigation();
-      this.#isNavigationRunning = false;
-    }
-    await super.afterEachStep?.(step, flow);
-  }
-
-  override async afterAllSteps(flow: UserFlow) {
-    if (this.#isTimespanRunning) {
-      await this.#lhFlow!.endTimespan();
-    }
-    await super.afterAllSteps?.(flow);
-  }
-}
+    
